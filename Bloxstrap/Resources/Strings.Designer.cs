76fb01fb12631d197990e79789b182d589aea24a
Boss1216 <86303500+upm-boss1216@users.noreply.github.com>
2025-02-23 15:35:53 +0700

    no more?

--- Bloxstrap/Resources/Strings.Designer.cs
+++ Bloxstrap/Resources/Strings.Designer.cs
@@ -578,6 +578,15 @@ namespace Bloxstrap.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Message.
+        /// </summary>
+        public static string Common_Message {
+            get {
+                return ResourceManager.GetString("Common.Message", resourceCulture);
+            }
+        }
+
+        /// <summary>
         ///   Looks up a localized string similar to Miscellaneous.
         /// </summary>
         public static string Common_Miscellaneous {
@@ -3641,6 +3650,15 @@ namespace Bloxstrap.Resources {
         public static string Menu_Integrations_Title {
             get {
                 return ResourceManager.GetString("Menu.Integrations.Title", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Load.
+        /// </summary>
+        public static string Menu_Load {
+            get {
+                return ResourceManager.GetString("Menu.Load", resourceCulture);
             }
         }
         
