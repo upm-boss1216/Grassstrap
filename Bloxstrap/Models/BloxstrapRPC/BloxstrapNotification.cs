972434ef88b7fc2487586a33ddcca9d18df21088
Boss1216 <86303500+upm-boss1216@users.noreply.github.com>
2025-02-26 23:39:36 +0700

    update

---
+++ Bloxstrap/Models/BloxstrapRPC/BloxstrapNotification.cs
@@ -0,0 +1,13 @@
+﻿namespace Bloxstrap.Models.BloxstrapRPC;
+
+class BloxstrapNotification
+{
+    [JsonPropertyName("title")]
+    public string? Title { get; set; }
+
+    [JsonPropertyName("message")]
+    public string? Message { get; set; }
+
+    [JsonPropertyName("duration")]
+    public int? Duration { get; set; }
+}
