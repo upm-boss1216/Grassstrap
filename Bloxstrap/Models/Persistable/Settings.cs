1348bc531d988a54e74b614a40feaae336815584
Boss1216 <86303500+upm-boss1216@users.noreply.github.com>
2025-02-26 21:15:13 +0700

    fix build

--- Bloxstrap/Models/Persistable/Settings.cs
+++ Bloxstrap/Models/Persistable/Settings.cs
@@ -2,44 +2,43 @@
 
 namespace Bloxstrap.Models.Persistable
 
-   public class Settings
+public class Settings
     {
-        // bloxstrap configuration
-        public BootstrapperStyle BootstrapperStyle { get; set; } = BootstrapperStyle.FluentAeroDialog;
-        public BootstrapperIcon BootstrapperIcon { get; set; } = BootstrapperIcon.IconBloxstrap;
-        public string BootstrapperTitle { get; set; } = App.ProjectName;
-        public string BootstrapperIconCustomLocation { get; set; } = "";
-        public Theme Theme { get; set; } = Theme.Default;
-        public bool CheckForUpdates { get; set; } = true;
-        public bool ConfirmLaunches { get; set; } = true;
-        public string Locale { get; set; } = "nil";
-        public bool ForceRobloxLanguage { get; set; } = false;
-        public bool UseFastFlagManager { get; set; } = true;
-        public bool WPFSoftwareRender { get; set; } = false;
-        public bool EnableAnalytics { get; set; } = false;
-        public bool UpdateRoblox { get; set; } = true;
-        public bool MultiInstanceLaunching { get; set; } = false;
-        public bool RenameClientToEuroTrucks2 { get; set; } = false;
-        public string ClientPath { get; set; } = Path.Combine(Paths.Base, "Roblox//Player");
-        public string Channel { get; set; } = Bloxstrap.RobloxInterfaces.Deployment.DefaultChannel;
-        public string ChannelHash { get; set; } = "";
-        public string DownloadingStringFormat { get; set; } = Strings.Bootstrapper_Status_Downloading + " {0} - {1}MB / {2}MB";
-        public string? SelectedCustomTheme { get; set; } = null;
+    // bloxstrap configuration
+    public BootstrapperStyle BootstrapperStyle { get; set; } = BootstrapperStyle.FluentAeroDialog;
+    public BootstrapperIcon BootstrapperIcon { get; set; } = BootstrapperIcon.IconBloxstrap;
+    public string BootstrapperTitle { get; set; } = App.ProjectName;
+    public string BootstrapperIconCustomLocation { get; set; } = "";
+    public Theme Theme { get; set; } = Theme.Default;
+    public bool CheckForUpdates { get; set; } = true;
+    public bool ConfirmLaunches { get; set; } = true;
+    public string Locale { get; set; } = "nil";
+    public bool ForceRobloxLanguage { get; set; } = false;
+    public bool UseFastFlagManager { get; set; } = true;
+    public bool WPFSoftwareRender { get; set; } = false;
+    public bool EnableAnalytics { get; set; } = false;
+    public bool UpdateRoblox { get; set; } = true;
+    public bool MultiInstanceLaunching { get; set; } = false;
+    public bool RenameClientToEuroTrucks2 { get; set; } = false;
+    public string ClientPath { get; set; } = Path.Combine(Paths.Base, "Roblox//Player");
+    public string Channel { get; set; } = Bloxstrap.RobloxInterfaces.Deployment.DefaultChannel;
+    public string ChannelHash { get; set; } = "";
+    public string DownloadingStringFormat { get; set; } = Strings.Bootstrapper_Status_Downloading + " {0} - {1}MB / {2}MB";
+    public string? SelectedCustomTheme { get; set; } = null;
 
-        // integration configuration
-        public bool EnableActivityTracking { get; set; } = true;
-        public bool UseDiscordRichPresence { get; set; } = true;
-        public bool CanGameMoveWindow { get; set; } = true;
-        public bool CanGameChangeColor { get; set; } = true;
-        public bool CanGameSetWindowTitle { get; set; } = true;
-        public int  WindowLogReadFPS { get; set; } = 60;
-        public ObservableCollection<long> WindowControlAllowedUniverses { get; set; } = new();
-        public bool HideRPCButtons { get; set; } = true;
-        public bool ShowAccountOnRichPresence { get; set; } = false;
-        public bool ShowServerDetails { get; set; } = false;
-        public ObservableCollection<CustomIntegration> CustomIntegrations { get; set; } = new();
+    // integration configuration
+    public bool EnableActivityTracking { get; set; } = true;
+    public bool UseDiscordRichPresence { get; set; } = true;
+    public bool CanGameMoveWindow { get; set; } = true;
+    public bool CanGameChangeColor { get; set; } = true;
+    public bool CanGameSetWindowTitle { get; set; } = true;
+    public int  WindowLogReadFPS { get; set; } = 60;
+    public ObservableCollection<long> WindowControlAllowedUniverses { get; set; } = new();
+    public bool HideRPCButtons { get; set; } = true;
+    public bool ShowAccountOnRichPresence { get; set; } = false;
+    public bool ShowServerDetails { get; set; } = false;
+    public ObservableCollection<CustomIntegration> CustomIntegrations { get; set; } = new();
 
-        // mod preset configuration
-        public bool UseDisableAppPatch { get; set; } = false;
-    }
+    // mod preset configuration
+    public bool UseDisableAppPatch { get; set; } = false;
 }
