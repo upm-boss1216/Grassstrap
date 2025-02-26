fe8f6c1f455ddc8116f94f9561a6d6f5c462d69f
Boss1216 <86303500+upm-boss1216@users.noreply.github.com>
2025-02-26 21:42:19 +0700

    update

--- Bloxstrap/UI/ViewModels/Settings/IntegrationsViewModel.cs
+++ Bloxstrap/UI/ViewModels/Settings/IntegrationsViewModel.cs
@@ -5,165 +5,172 @@ using Microsoft.Win32;
 
 using CommunityToolkit.Mvvm.Input;
 
-namespace Bloxstrap.UI.ViewModels.Settings
+namespace Bloxstrap.UI.ViewModels.Settings;
+
+public class IntegrationsViewModel : NotifyPropertyChangedViewModel
 {
-    public class IntegrationsViewModel : NotifyPropertyChangedViewModel
+    public ICommand AddIntegrationCommand => new RelayCommand(AddIntegration);
+
+    public ICommand DeleteIntegrationCommand => new RelayCommand(DeleteIntegration);
+
+    public ICommand BrowseIntegrationLocationCommand => new RelayCommand(BrowseIntegrationLocation);
+
+    private void AddIntegration()
     {
-        public ICommand AddIntegrationCommand => new RelayCommand(AddIntegration);
-
-        public ICommand DeleteIntegrationCommand => new RelayCommand(DeleteIntegration);
-
-        public ICommand BrowseIntegrationLocationCommand => new RelayCommand(BrowseIntegrationLocation);
-
-        private void AddIntegration()
+        CustomIntegrations.Add(new CustomIntegration()
         {
-            CustomIntegrations.Add(new CustomIntegration()
-            {
-                Name = Strings.Menu_Integrations_Custom_NewIntegration
-            });
+            Name = Strings.Menu_Integrations_Custom_NewIntegration
+        });
 
-            SelectedCustomIntegrationIndex = CustomIntegrations.Count - 1;
+        SelectedCustomIntegrationIndex = CustomIntegrations.Count - 1;
 
-            OnPropertyChanged(nameof(SelectedCustomIntegrationIndex));
-            OnPropertyChanged(nameof(IsCustomIntegrationSelected));
-        }
-
-        private void DeleteIntegration()
-        {
-            if (SelectedCustomIntegration is null)
-                return;
-
-            CustomIntegrations.Remove(SelectedCustomIntegration);
-
-            if (CustomIntegrations.Count > 0)
-            {
-                SelectedCustomIntegrationIndex = CustomIntegrations.Count - 1;
-                OnPropertyChanged(nameof(SelectedCustomIntegrationIndex));
-            }
-
-            OnPropertyChanged(nameof(IsCustomIntegrationSelected));
-        }
-
-        private void BrowseIntegrationLocation()
-        {
-            if (SelectedCustomIntegration is null)
-                return;
-
-            var dialog = new OpenFileDialog
-            {
-                Filter = $"{Strings.Menu_AllFiles}|*.*"
-            };
-
-            if (dialog.ShowDialog() != true)
-                return;
-
-            SelectedCustomIntegration.Name = dialog.SafeFileName;
-            SelectedCustomIntegration.Location = dialog.FileName;
-            OnPropertyChanged(nameof(SelectedCustomIntegration));
-        }
-
-        public bool ActivityTrackingEnabled
-        {
-            get => App.Settings.Prop.EnableActivityTracking;
-            set
-            {
-                App.Settings.Prop.EnableActivityTracking = value;
-
-                if (!value)
-                {
-                    ShowServerDetailsEnabled = value;
-                    DisableAppPatchEnabled = value;
-                    DiscordActivityEnabled = value;
-                    GameWindowControlEnabled = value;
-                    GameWindowTitleControlEnabled = value;
-                    DiscordActivityJoinEnabled = value;
-
-                    OnPropertyChanged(nameof(ShowServerDetailsEnabled));
-                    OnPropertyChanged(nameof(DisableAppPatchEnabled));
-                    OnPropertyChanged(nameof(DiscordActivityEnabled));
-                    OnPropertyChanged(nameof(GameWindowControlEnabled));
-                    OnPropertyChanged(nameof(GameWindowTitleControlEnabled));
-                    OnPropertyChanged(nameof(DiscordActivityJoinEnabled));
-                }
-            }
-        }
-
-        public bool ShowServerDetailsEnabled
-        {
-            get => App.Settings.Prop.ShowServerDetails;
-            set => App.Settings.Prop.ShowServerDetails = value;
-        }
-
-        public bool PlayerLogsEnabled
-        {
-            get => App.FastFlags.GetPreset("Players.LogLevel") == "trace"; // we r using this to determine if its enabled
-            set {
-                App.FastFlags.SetPreset("Players.LogLevel", value ? "trace" : null);
-                App.FastFlags.SetPreset("Players.LogPattern", value ? "ExpChat/mountClientApp" : null);
-            }
-        }
-
-        public bool DiscordActivityEnabled
-        {
-            get => App.Settings.Prop.UseDiscordRichPresence;
-            set
-            {
-                App.Settings.Prop.UseDiscordRichPresence = value;
-
-                if (!value)
-                {
-                    DiscordActivityJoinEnabled = value;
-                    DiscordAccountOnProfile = value;
-                    OnPropertyChanged(nameof(DiscordActivityJoinEnabled));
-                    OnPropertyChanged(nameof(DiscordAccountOnProfile));
-                }
-            }
-        }
-
-        public bool GameWindowControlEnabled
-        {
-            get => App.Settings.Prop.CanGameMoveWindow;
-            set => App.Settings.Prop.CanGameMoveWindow = value;
-        }
-
-        public int GameWindowLogFPSInterval
-        {
-            get => App.Settings.Prop.WindowLogReadFPS;
-            set => App.Settings.Prop.WindowLogReadFPS = value;
-        }
-
-        public bool GameWindowTitleControlEnabled
-        {
-            get => App.Settings.Prop.CanGameSetWindowTitle;
-            set => App.Settings.Prop.CanGameSetWindowTitle = value;
-        }
-
-        public bool DiscordActivityJoinEnabled
-        {
-            get => !App.Settings.Prop.HideRPCButtons;
-            set => App.Settings.Prop.HideRPCButtons = !value;
-        }
-
-        public bool DiscordAccountOnProfile
-        {
-            get => App.Settings.Prop.ShowAccountOnRichPresence;
-            set => App.Settings.Prop.ShowAccountOnRichPresence = value;
-        }
-
-        public bool DisableAppPatchEnabled
-        {
-            get => App.Settings.Prop.UseDisableAppPatch;
-            set => App.Settings.Prop.UseDisableAppPatch = value;
-        }
-
-        public ObservableCollection<CustomIntegration> CustomIntegrations
-        {
-            get => App.Settings.Prop.CustomIntegrations;
-            set => App.Settings.Prop.CustomIntegrations = value;
-        }
-
-        public CustomIntegration? SelectedCustomIntegration { get; set; }
-        public int SelectedCustomIntegrationIndex { get; set; }
-        public bool IsCustomIntegrationSelected => SelectedCustomIntegration is not null;
+        OnPropertyChanged(nameof(SelectedCustomIntegrationIndex));
+        OnPropertyChanged(nameof(IsCustomIntegrationSelected));
     }
+
+    private void DeleteIntegration()
+    {
+        if (SelectedCustomIntegration is null)
+            return;
+
+        CustomIntegrations.Remove(SelectedCustomIntegration);
+
+        if (CustomIntegrations.Count > 0)
+        {
+            SelectedCustomIntegrationIndex = CustomIntegrations.Count - 1;
+            OnPropertyChanged(nameof(SelectedCustomIntegrationIndex));
+        }
+
+        OnPropertyChanged(nameof(IsCustomIntegrationSelected));
+    }
+
+    private void BrowseIntegrationLocation()
+    {
+        if (SelectedCustomIntegration is null)
+            return;
+
+        var dialog = new OpenFileDialog
+        {
+            Filter = $"{Strings.Menu_AllFiles}|*.*"
+        };
+
+        if (dialog.ShowDialog() != true)
+            return;
+
+        SelectedCustomIntegration.Name = dialog.SafeFileName;
+        SelectedCustomIntegration.Location = dialog.FileName;
+        OnPropertyChanged(nameof(SelectedCustomIntegration));
+    }
+
+    public bool ActivityTrackingEnabled
+    {
+        get => App.Settings.Prop.EnableActivityTracking;
+        set
+        {
+            App.Settings.Prop.EnableActivityTracking = value;
+
+            if (!value)
+            {
+                ShowServerDetailsEnabled = value;
+                DisableAppPatchEnabled = value;
+                DiscordActivityEnabled = value;
+                GameWindowControlEnabled = value;
+                GameWindowColorEnabled = value;
+                GameWindowTitleControlEnabled = value;
+                DiscordActivityJoinEnabled = value;
+
+                OnPropertyChanged(nameof(ShowServerDetailsEnabled));
+                OnPropertyChanged(nameof(DisableAppPatchEnabled));
+                OnPropertyChanged(nameof(DiscordActivityEnabled));
+                OnPropertyChanged(nameof(GameWindowControlEnabled));
+                OnPropertyChanged(nameof(GameWindowColorEnabled));
+                OnPropertyChanged(nameof(GameWindowTitleControlEnabled));
+                OnPropertyChanged(nameof(DiscordActivityJoinEnabled));
+            }
+        }
+    }
+
+    public bool ShowServerDetailsEnabled
+    {
+        get => App.Settings.Prop.ShowServerDetails;
+        set => App.Settings.Prop.ShowServerDetails = value;
+    }
+
+    public bool PlayerLogsEnabled
+    {
+        get => App.FastFlags.GetPreset("Players.LogLevel") == "trace"; // we r using this to determine if its enabled
+        set {
+            App.FastFlags.SetPreset("Players.LogLevel", value ? "trace" : null);
+            App.FastFlags.SetPreset("Players.LogPattern", value ? "ExpChat/mountClientApp" : null);
+        }
+    }
+
+    public bool DiscordActivityEnabled
+    {
+        get => App.Settings.Prop.UseDiscordRichPresence;
+        set
+        {
+            App.Settings.Prop.UseDiscordRichPresence = value;
+
+            if (!value)
+            {
+                DiscordActivityJoinEnabled = value;
+                DiscordAccountOnProfile = value;
+                OnPropertyChanged(nameof(DiscordActivityJoinEnabled));
+                OnPropertyChanged(nameof(DiscordAccountOnProfile));
+            }
+        }
+    }
+
+    public bool GameWindowControlEnabled
+    {
+        get => App.Settings.Prop.CanGameMoveWindow;
+        set => App.Settings.Prop.CanGameMoveWindow = value;
+    }
+
+    public bool GameWindowColorEnabled
+    {
+        get => App.Settings.Prop.CanGameChangeColor;
+        set => App.Settings.Prop.CanGameChangeColor = value;
+    }
+
+    public int GameWindowLogFPSInterval
+    {
+        get => App.Settings.Prop.WindowLogReadFPS;
+        set => App.Settings.Prop.WindowLogReadFPS = value;
+    }
+
+    public bool GameWindowTitleControlEnabled
+    {
+        get => App.Settings.Prop.CanGameSetWindowTitle;
+        set => App.Settings.Prop.CanGameSetWindowTitle = value;
+    }
+
+    public bool DiscordActivityJoinEnabled
+    {
+        get => !App.Settings.Prop.HideRPCButtons;
+        set => App.Settings.Prop.HideRPCButtons = !value;
+    }
+
+    public bool DiscordAccountOnProfile
+    {
+        get => App.Settings.Prop.ShowAccountOnRichPresence;
+        set => App.Settings.Prop.ShowAccountOnRichPresence = value;
+    }
+
+    public bool DisableAppPatchEnabled
+    {
+        get => App.Settings.Prop.UseDisableAppPatch;
+        set => App.Settings.Prop.UseDisableAppPatch = value;
+    }
+
+    public ObservableCollection<CustomIntegration> CustomIntegrations
+    {
+        get => App.Settings.Prop.CustomIntegrations;
+        set => App.Settings.Prop.CustomIntegrations = value;
+    }
+
+    public CustomIntegration? SelectedCustomIntegration { get; set; }
+    public int SelectedCustomIntegrationIndex { get; set; }
+    public bool IsCustomIntegrationSelected => SelectedCustomIntegration is not null;
 }
